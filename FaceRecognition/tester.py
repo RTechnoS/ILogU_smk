8d589686025ffd85cf897208c5e75d48aba2e2cd
rtechnos <rusmantobyaktasiregar@gmail.com>
2020-11-29 17:55:17 +0700

    menambah FaceRecognition modul

---
+++ FaceRecognition/tester.py
@@ -0,0 +1,9 @@
+import cv2
+import os
+import numpy as np
+import faceRecognition as fr
+
+#Comment belows lines when running this program second time.Since it saves training.yml file in directory
+faces,faceID=fr.labels_for_training_data('trainingImages')
+face_recognizer=fr.train_classifier(faces,faceID)
+face_recognizer.write('trainingData.yml')
