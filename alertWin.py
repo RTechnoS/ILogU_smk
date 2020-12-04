c9c553f08937a94430a5caf1cf26ca2422b5dd4d
rtechnos <rusmantobyaktasiregar@gmail.com>
2020-12-04 18:05:30 +0700

    menambah fungsi lacakSiswa(belum dicobna full

--- alertWin.py
+++ alertWin.py
@@ -36,102 +36,10 @@ class Main:
 		print('Melacak')
 		self.__frmLog = tk.Toplevel(self.dash, bg='red')
 		self.__frmLog.geometry('500x500')
-		#self.__frmLog.pack(side=tk.BOTTOM, fill=tk.X, padx=20, pady=15)
-		#tunggu.pack()
 		self.logWin()
 		self.__frmLog.mainloop()
 
 	def logWin(self):
-		__sql = 'SELECT * from logSiswa where nama=%s ORDER BY waktu ASC'
-		__d = (self.namaKorban.get(),)
-		#print(self.namaKorban.get())
-		c.execute(__sql,__d)
-		__isData = c.fetchall()
-		Main.logKorban = __isData
-		#print(Main.logKorban)
-		self.periksaTeman()
-	
-	def periksaTeman(self):
-		col = ('id', 'Nama', 'Tanggal', 'Waktu', 'Lokasi', 'Terdekat', 'Coor')
-		logTerdekat = []
-		for log in Main.logKorban:
-			idLog ,nama, tanggal, waktu, lokasi, terdekat, coor = log
-			#print(str(waktu))
-			__sql = "SELECT * from logSiswa where nama!='{}' and tanggal='{}' and lokasi='{}' and waktu like '%{}%' ORDER BY waktu ASC".format(self.namaKorban.get() ,tanggal,lokasi,':'.join(str(waktu).split(':')[0:-1]))
-			#print("SELECT * from logSiswa where tanggal={} and lokasi={} and waktu like '%{}%' ORDER BY waktu ASC".format(tanggal,lokasi,':'.join(str(waktu).split(':')[0:-1])))
-			c.execute(__sql)
-			if log[5] != '' and log[5] != None:
-				getTerdekat = log[5]
-				for i in getTerdekat.split(','):
-					logTerdekat.append(i)
-
-			#print(getTerdekat)
-			dataTeman = c.fetchall()
-			if len(dataTeman) != 0:
-				Main.logTeman.append(dataTeman[0])
-
-		
-				#Main.logTerdekat.append()
-		#print(Main.logTerdekat)
-		#logTerdekat = DataFrame(Main.logTerdekat.value_counts(), columns)
-				
-		
-		dataLog = DataFrame(Main.logTeman, columns=col)
-		logNear = list(set(logTerdekat))
-		for nameLog in logNear:
-			Main.logTerdekat[nameLog] = logTerdekat.count(nameLog)
-			#print('Nama : ', nameLog, ' Jumlah : ',logTerdekat.count(nameLog))
-		print(list(Main.logTerdekat.values()), list(Main.logTerdekat.keys()))
-		#dataLog = 
-		#print(dataLog.Terdekat.split(','))
-		#print(dataLog.Terdekat.value_counts())
-		t = (dataLog.Nama.value_counts())
-		#print(dir(t))
-		namaOrg = t.keys()
-		jumlahLog = t.values
-		plt.figure(figsize=(7,5))
-
-		fig, ax = plt.subplots()
-
-		#fig.suptitle('Vertically stacked subplots')
-		# axs[0].plot(x, y)
-		# axs[1].plot(x, -y)
-		# ax = plt.subplot(111)
-		# fig, ax = plt.subplots()
-		# rects1 = ax.bar(namaOrg, jumlahLog, width=0.3, align='center', color='blue', label='testing bro')
-		# rects2 = ax.bar(Main.logTerdekat.keys(), Main.logTerdekat.values(), width=0.3, align='center', color='red')
-
-		def autolabel(rects):
-		    """Attach a text label above each bar in *rects*, displaying its height."""
-		    for rect in rects:
-		        height = rect.get_height()
-		        ax.annotate('{}'.format(height),
-		                    xy=(rect.get_x() + rect.get_width() / 2, height),
-		                    xytext=(0, 3),  # 3 points vertical offset
-		                    textcoords="offset points",
-		                    ha='center', va='bottom')
-
-		plt1 = ax.bar(namaOrg, jumlahLog, width=0.3, align='center', color='blue', label='testing bro')
-
-		plt2 = ax.bar(Main.logTerdekat.keys(), Main.logTerdekat.values(), width=0.3, align='center', color='red')
-		autolabel(plt1)
-		autolabel(plt2)
-		#plt.xticks()
-		plt.show()
-		print('Yang pernah bertemu', dataLog.Nama.unique())
-		print(dataLog.Nama.value_counts())
-				#print(dataLog.Nama.value_counts())
-				#nama = []
-				#for dttmn in Main.logTeman:
-
-				# 	#logTeman 
-				# 	nama.append(dttmn[1])
-				# print(nama)
-
-				#print(idLog ,nama, tanggal, waktu, lokasi, terdekat, coor)
-
-
-
-
-
-
+		import lacakSiswa as lck
+		dd = lck.lacak(self.namaKorban.get())
+		dd.showPlt()
--- lacakSiswa.py
+++ lacakSiswa.py
@@ -8,8 +8,78 @@ c = fungsi.c
 
 class lacak:
 	def __init__(self, nama):
-		namaKorban = 'karim'
+		self.namaKorban = 'karim'
 		self.logKorban = []
 		self.logTerdekat = []
 		self.col = ['id', 'Nama', 'Tanggal', 'Waktu', 'Lokasi', 'Terdekat', 'Coor']
 		self.logTeman = []
+		self.ambilKorban()
+	
+	def ambilKorban(self):
+		c.execute("SELECT * from logSiswa where nama='{}' ORDER BY waktu ASC".format(self.namaKorban))
+		__isData = c.fetchall()
+		self.logKorban = __isData
+
+		for log in self.logKorban:
+		    idLog ,nama, tanggal, waktu, lokasi, terdekat, coor = log
+		    __sql = "SELECT * from logSiswa where nama!='{}' and tanggal='{}' and lokasi='{}' and waktu like '%{}%' ORDER BY waktu ASC".format(namaKorban ,tanggal,lokasi,':'.join(str(waktu).split(':')[0:-1]))
+		    c.execute(__sql)
+
+		    if log[5] != '' and log[5] != None:
+		        getTerdekat = log[5]
+		        for i in getTerdekat.split(','):
+		            self.logTerdekat.append(i)
+		    dataTeman = c.fetchall()
+		    if len(dataTeman) != 0:
+		        self.logTeman.append(dataTeman[0])
+		self.prosesData()
+
+	def prosesData(self):
+		self.dataF = pd.DataFrame(columns=('nama', 'oneLoc', 'near', 'hari'))
+		self.logKorban = pd.DataFrame(self.logTeman, columns=col) # log si korban covid
+		self.logTeman = pd.DataFrame(self.logTeman, columns=col) # log teman yang pernah satu lokasi dengan korban
+		self.logKorban.drop('id',axis=1, inplace=True) 
+		self.logTeman.drop('id',axis=1, inplace=True)
+
+		for g in set(self.logKorban.Nama.unique()):
+			self.dataF = self.dataF.append({'nama':g, 'near':self.logTerdekat.count(g),  'hari':list(self.logTeman.Tanggal[self.logTeman['Nama'] == g].unique())} , ignore_index=True)
+		print(self.logKorban)
+		tanggalKorban = self.logKorban.sort_values(by='Tanggal').Tanggal.unique() # semua tanggal saat korban terlacak
+		
+		d = list()
+		for tgl in tanggalKorban: 
+			Lok = self.logKorban[self.logTeman.Tanggal == tgl].Lokasi.unique() #semua tempat(unik) yang delalui korban ex=2020-04-25
+			for lk in Lok: #
+				#print(tgl, lk)
+				jam = self.logKorban[self.logKorban.Lokasi == lk].sort_values(by='Waktu').Waktu
+				for p in self.logTeman[self.logTeman.Tanggal == tgl].sort_values(by='Waktu')['Nama'].unique():
+		            d.append(p)
+
+		self.dataF = self.dataF.set_index('nama')
+		for na in self.dataF.index:
+		    self.dataF.at[na, 'oneLoc'] = d.count(na)
+		print(self.dataF)
+
+	def showPlt(self):
+		plt.figure(figsize=(20,10))
+		n = self.dataF.index
+		locA= self.dataF['oneLoc']
+		neaR = self.dataF['near']
+		fig, ax = plt.subplots()
+		index = np.arange(len(n))
+		pl1 = ax.bar(index, locA, bar_width, color='r',
+		                label='Jumlah Satu Lokasi')
+
+		pl2 = ax.bar(index+0.35, neaR, bar_width, color='b',
+		                label='Jumlah Berdekatan')
+
+		ax.set_xlabel('Nama')
+		ax.set_ylabel('Berapa Kali')
+		ax.set_title('Static interkasi dengan korban')
+		ax.set_xticks(index + bar_width / 2)
+		ax.set_xticklabels(dataF.index)
+		ax.legend()
+		autolabel(pl1)
+		autolabel(pl2)
+		# plt.savefig("tes.png")
+		plt.show()
